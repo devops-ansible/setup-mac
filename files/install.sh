9bb4631c5d83c5370b4622bf83e375429f55c69b
Christian Walonka <cwalonka@Mac-mini-von-Christian.local>
2021-01-30 11:27:39 +0100

    added check for already installed tools

--- files/install.sh
+++ files/install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 xcode-select --install
 sudo xcodebuild -license accept
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update
-brew install ansible
+if ! command -v brew &> /dev/null
+then
+    echo "brew could not be found and will be installed now"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! command -v ansible &> /dev/null
+then
+    echo "ansible could not be found and will be installed now"
+    brew update
+    brew install ansible
+fi
+
 cd ../
 ansible-galaxy install -r requirements.yml
 ansible-playbook -K main.yml
